48957cc27256003e81cb6f20fef96258eaae8fa4
Vitor Bianchini de Quadros <vitorbiqua@outlook.com>
2022-07-23 12:18:13 -0300

    Começando a contruir as configurações do BOT

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,42 +1,37 @@
 import { CommandInteraction, Message } from "discord.js";
-import { Discord, Slash, SlashChoice, SlashChoiceType, SlashOption } from "discordx";
+import { Discord, Slash, SlashChoice, SlashOption } from "discordx";
 
-const languages = [
-  "portuguese",
-  "english",
-  "french",
-  "spanish",
-  "indian"
-]
+const languages = ["portuguese", "english", "french", "spanish", "indian"];
 
 // const idioma: SlashChoiceType[] = [
 //   {name: "português", value: "Portugês"},
 //   {name: "inglês", value:"Inglês"}
 // ]
 
-const idioma = [
-  "portuguese",
-  "english",
-]
+const idioma = ["portuguese", "english"];
 
 @Discord()
 export class Config {
-    @Slash("config")
+    @Slash("config", {
+        description:
+            "Configure o seu Voice Transcription, selecione idioma e legenda",
+    })
     config(
         @SlashChoice(...idioma)
         @SlashOption("idioma", { description: "Lingua falada" })
         idioma: string,
-        
-        @SlashChoice( ...languages )
+
+        @SlashChoice(...languages)
         @SlashOption("legenda", { description: "Legenda gerada" })
         legenda: string,
-
         interaction: CommandInteraction
-      ): void {
-        const idiomaFormatado = idioma[0].toUpperCase() + idioma.substring(1)
-        const legendaFormatada = legenda[0].toUpperCase() + legenda.substring(1)
+    ): void {
+        const idiomaFormatado = idioma[0].toUpperCase() + idioma.substring(1);
+        const legendaFormatada =
+            legenda[0].toUpperCase() + legenda.substring(1);
 
-
-        interaction.reply(`Idioma: ${idiomaFormatado} Legenda: ${legendaFormatada}`);
-      };
+        interaction.reply(
+            `Idioma: ${idiomaFormatado} Legenda: ${legendaFormatada}`
+        );
+    }
 }
